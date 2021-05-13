3c8d071feef1f09b07eef2c49659fe91c27023c7
bohemianabe <agarrido@gmail.com>
2021-05-13 18:55:54 -0400

    final push with animation touches added.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,7 @@ import logo from '../img/logo.svg';
 import {fetchSearch} from '../actions/gamesAction'
 import {useDispatch} from 'react-redux';
 import {useState} from 'react'
+import {fadeIn} from '../animations'
 
 
 const Nav = () => {
@@ -25,7 +26,7 @@ const Nav = () => {
         dispatch({type: "CLEAR_SEARCHED"})
     }
     return(
-        <StyledNav>
+        <StyledNav variants={fadeIn} initial="hidden" animate="show">
             <Logo onClick={clearSearched}>
                 <img src={logo} alt="logo" />
                 <h1>Ignite</h1>
