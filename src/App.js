7b80d84bb0bb5dc3f59d389cbbc8400a36ef2bf9
bohemianabe <agarrido@gmail.com>
2021-05-12 20:35:03 -0400

    nav header added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Home from './pages/Home'
 import GlobalStyles from './components/GlobalStyles'
 import {Route} from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
 
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       {/* this path will render out the home component if it's on / or the game id */}
       <Route path={['/game/:id', '/']}>
       <Home />
